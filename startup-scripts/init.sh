323987bb73ac4e82e36103b453a14a79ebbdaff6
Peter van Dijk <peter@7bits.nl>
2012-04-14 20:34:15 +0200

    update scripts to make it easier to start daemons besides ssh

--- startup-scripts/init.sh
+++ startup-scripts/init.sh
@@ -6,18 +6,18 @@ case "$1" in
                 mount -t devpts none /etc/zyxel/storage/sysvol/.debian/root/dev/pts
                 mount --bind /etc/zyxel/storage/sysvol /etc/zyxel/storage/sysvol/.debian/root/data
                 cp /etc/resolv.conf /etc/zyxel/storage/sysvol/.debian/root/etc
-                chroot /etc/zyxel/storage/sysvol/.debian/root /etc/init.d/ssh start
+                chroot /etc/zyxel/storage/sysvol/.debian/root /etc/chroot-init $1
                 ;;
         stop|shutdown)
+                chroot /etc/zyxel/storage/sysvol/.debian/root /etc/chroot-init $1
                 umount /etc/zyxel/storage/sysvol/.debian/root/proc
                 umount /etc/zyxel/storage/sysvol/.debian/root/sys
                 umount /etc/zyxel/storage/sysvol/.debian/root/dev/pts
                 umount /etc/zyxel/storage/sysvol /etc/zyxel/storage/sysvol/.debian/root/data
-                chroot /etc/zyxel/storage/sysvol/.debian/root /etc/init.d/ssh stop
-                ;;                              
+                ;;
         *)
                 exit 0
-                ;;              
+                ;;
 esac
 
 exit 0
